5551b452cd1c49af43420f796fe19792fab61a87
marvel16 <https://github.com/marvel16>
2016-01-19 08:56:32 +0200

    Initial commit, with some drafts

---
+++ SocketsChat_WPF/BaseViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using SocketsChat_WPF.Annotations;
+
+namespace SocketsChat_WPF
+{
+    class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    class ViewModel : BaseViewModel
+    {
+         public string 
+    }
+}
