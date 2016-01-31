291b0e52b8f8ede3dc29957d1bea8f07b9943e85
marvel16 <https://github.com/marvel16>
2016-01-31 21:29:48 +0200

    console server added for testing

--- SocketsChat_WPF/App.xaml.cs
+++ SocketsChat_WPF/App.xaml.cs
@@ -16,12 +16,8 @@ namespace SocketsChat_WPF
     {
         public void OnStartUp(object sender, StartupEventArgs e)
         {
-            var msgs = new List<UserMessageViewModel>
-            {
-                new UserMessageViewModel() {Message = "ASDSAD SAD A123213213213213213D ASD SAD ASD ADS "},
-                new UserMessageViewModel() {Message = "ASDSADdssdsadasdasdD ASD ADS SAD ASD SAD ASD ADS "},
-                new UserMessageViewModel() {Message = "ASDSbdbdbdb basbdbdbd ASD ASD ADS SAD ASD SAD ASD ADS "},
-            };
+            Client client = new Client();
+            client.Connect("localhost", 50000);
 
             ClientMainWindow view = new ClientMainWindow();
             //view.DataContext = new UserMessagesViewModel();
--- SocketsChat_WPF/Client.cs
+++ SocketsChat_WPF/Client.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Net.Sockets;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using CustomNetworkExtensions;
@@ -10,7 +12,36 @@ namespace SocketsChat_WPF
 {
     class Client
     {
-        TcpClient _Client;
-        public event Action<MessageData> MessageReceived;
+        private TcpClient _Client;
+
+        private NetworkStream Stream => _Client?.GetStream();
+
+        public event Action Connected;
+
+        public async Task Connect(string ip, int port)
+        {
+            _Client?.Close();
+            _Client = new TcpClient();
+
+            await _Client.ConnectAsync(ip, port);
+        }
+
+        public async Task<MessageData> ReadMessage()
+        {
+            byte[] messageLength = new byte[sizeof(int)];
+            await Stream?.ReadAsync(messageLength, 0, messageLength.Length);
+            int len = BitConverter.ToInt32(messageLength, 0);
+            byte[] messageBytes = new byte[len];
+            await Stream?.ReadAsync(messageBytes, 0, len);
+
+            return (MessageData)messageBytes.ByteArrayToObject();
+        }
+
+        public async Task WriteMessage(MessageData message)
+        {
+            byte[] bytes = message.ToByteArray();
+            Stream?.WriteAsync(bytes, 0, bytes.Length);
+        }
+        
     }
 }
