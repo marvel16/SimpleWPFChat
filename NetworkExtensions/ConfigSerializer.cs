18d88d161a136e1c836c1ab562e676516a0aaef4
marvel16 <https://github.com/marvel16>
2016-03-13 22:38:00 +0200

    added missing ConfigSerializer to git

---
+++ NetworkExtensions/ConfigSerializer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace NetworkExtensions
+{
+    public class CfgMan
+    {
+        private static readonly string CurrentDir = Directory.GetCurrentDirectory();
+
+        public static void Serialize<T>(T cfg, string path)
+        {
+            using (FileStream fs = new FileStream(CurrentDir + @"\" + path, FileMode.Create))
+            {
+                XmlWriterSettings xmlSettings = new XmlWriterSettings { Indent = true };
+                XmlWriter writer = XmlWriter.Create(fs, xmlSettings);
+                XmlSerializer ser = new XmlSerializer(typeof(T));
+                ser.Serialize(writer, cfg);
+            }
+        }
+        public static void Deserialize<T>(ref T cfg, string path)
+        {
+            if (!File.Exists(CurrentDir + @"\" + path))
+                return;
+           
+            using (FileStream fs = new FileStream(CurrentDir + @"\" + path, FileMode.Open))
+            {
+                XmlReader reader = XmlReader.Create(fs);
+                XmlSerializer ser = new XmlSerializer(typeof(T));
+                cfg = (T)ser.Deserialize(reader);
+            }
+        }
+    }
+}
